a726652f8cdb3132a2c01b9d7f6cc323b846edad
Lakshya Raikwal <72344300+Lakshya123-cyber@users.noreply.github.com>
2021-11-07 21:40:24 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
+// !Owl carousel Responsiveness
 
+const responsive = {
+    0:{
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+$(document).ready(function(){
+    // !Toggle Menu
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    // *Click event on toggle*
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
+
+    // !Owl carousel for blog
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$(".owl-nav .owl-nav-prev"), $(".owl-nav .owl-nav-next")],
+        responsive: responsive,
+    });
+
+    // !Click to scroll top
+    $('.move-up span').click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+    })
+
+    // !AOS Instance
+    AOS.init();
+})
